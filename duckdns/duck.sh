a3b9c53d46d526b48220c9df81ab8234c86bc03e
Quentin Bonnet <quentin.bonnet.work@gmail.com>
2024-01-19 17:51:22 +0100

    Verbose output in duckdns curl

--- duckdns/duck.sh
+++ duckdns/duck.sh
@@ -1,1 +1,1 @@
-echo url="https://www.duckdns.org/update?domains=lesbonin&token=<DUCK_DNS_TOKEN_HERE>&ip=" | curl -k -o ~/duckdns/duck.log -K -
+echo url="https://www.duckdns.org/update?domains=lesbonin&token=2315c689-7a06-40df-9ce2-5a9ad25429c1&ip=" | curl -v -k -K - &>> duck.log
