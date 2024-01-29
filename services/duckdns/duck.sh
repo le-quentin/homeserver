517afc0ee91db3d2c0a7d521f7cd55f09f1e5eb9
Quentin Bonnet <quentin.bonnet.work@gmail.com>
2024-01-29 22:23:16 +0100

    Setup duckdns cron job

--- services/duckdns/duck.sh
+++ services/duckdns/duck.sh
@@ -1,1 +1,1 @@
-echo url="https://www.duckdns.org/update?domains=lesbonin&token=2315c689-7a06-40df-9ce2-5a9ad25429c1&ip=" | curl -v -k -K - &>> duck.log
+echo url="https://www.duckdns.org/update?domains=lesbonin&token=$1&ip=" | curl -v -k -K - 
