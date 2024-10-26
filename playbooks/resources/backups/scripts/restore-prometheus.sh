7296f582f031363abc9dabb0e9b1a0440b933895
Quentin Bonnet <quentin@bonnet.software>
2024-10-26 02:32:44 +0200

    Add backup restore scripts

---
+++ playbooks/resources/backups/scripts/restore-prometheus.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+set -e 
+
+fail() {
+    echo "$1" >&2
+    exit "${2-1}"
+}
+
+BACKUP_ARCHIVE_PATH="$1"
+BACKUP_NAME="$(basename $BACKUP_ARCHIVE_PATH)"
+
+file $BACKUP_ARCHIVE_PATH | grep -q 'gzip compressed' || fail "Argument must be a tar.gz archive containing a backup"
+
+echo "This will erase the current container data completely. Are you sure?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) break;;
+        No ) exit;;
+    esac
+done
+
+echo "Stopping the container..."
+docker compose -f ~/services/docker-volume-backup/compose.yaml rm -f
+docker compose -f ~/monitoring/compose.yaml down prometheus
+docker compose -f ~/monitoring/compose.yaml rm prometheus
+
+echo "Recreating the docker volume..."
+docker volume rm prometheus-data
+docker volume create prometheus-data
+echo "Unzipping the backup to the new volume"
+docker run --rm -it -v prometheus-data:/backup/prometheus -v ~/backups/:/archive:ro alpine tar -xvzf /archive/$BACKUP_NAME
+echo "Restarting the container..."
+docker compose -f ~/monitoring/compose.yaml up -d
+echo "Completed."
