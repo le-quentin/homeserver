89ce3839fcafe67824061c4bfdd444a51a625a86
Quentin Bonnet <quentin@bonnet.software>
2024-10-30 13:24:51 +0100

    Very basic syntax check before running tests

--- test/run_all.sh
+++ test/run_all.sh
@@ -5,6 +5,10 @@ PROJECT_DIR="$SCRIPT_DIR/.."
 
 cd "$SCRIPT_DIR"
 
+set -e
+ansible-playbook -i "$PROJECT_DIR/inventories/prod" --syntax-check "$PROJECT_DIR/playbooks/all.yaml"
+set +e
+
 vagrant destroy -f --no-tty
 
 vagrant up --no-tty && \
