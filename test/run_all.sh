718fd41aeec0e695d2ab98c3db1066e9aae4eede
Quentin Bonnet <quentin@bonnet.software>
2024-10-30 13:10:50 +0100

    Fix test env

--- test/run_all.sh
+++ test/run_all.sh
@@ -1,12 +1,14 @@
 #!/bin/zsh
 
-SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+SCRIPT_DIR="${0:A:h}"
 PROJECT_DIR="$SCRIPT_DIR/.."
+
+cd "$SCRIPT_DIR"
 
 vagrant destroy -f --no-tty
 
-vagrant up --no-tty && /
-ssh-keygen -R 192.168.121.101 && /
+vagrant up --no-tty && \
+ssh-keygen -R 192.168.121.101 && \
 
 ansible-playbook -e 'host_key_checking=False' -i "$PROJECT_DIR/inventories/test" "$PROJECT_DIR/playbooks/all.yaml" --ssh-common-args='-o StrictHostKeyChecking=no'
 
