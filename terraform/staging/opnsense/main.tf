ad337168ca1d21e4d793fdacc7d6f24dad3bb5cf
Quentin Bonnet <quentin@bonnet.software>
2024-11-29 01:40:55 +0100

    Opnsense terraform in staging

---
+++ terraform/staging/opnsense/main.tf
@@ -0,0 +1,17 @@
+module "opnsense" {
+  source = "../../modules/opnsense"
+
+  environment = "staging"
+  pool        = "staging"
+
+  vm_ids_offset = 1000
+
+  opnsense_vm_template_id = 1998
+
+  bridge_lan_interface             = "vmbr1001"
+  bridge_lan_network_ip            = "10.142.1.100"
+  bridge_management_lan_interface  = "vmbr1100"
+  bridge_management_lan_network_ip = "10.143.1.100"
+  legacy_vlan_network_ip           = "10.142.200.1"
+
+}
---
+++ terraform/staging/opnsense/provider.secrets.auto.tfvars
@@ -0,0 +1,1 @@
+../provider.secrets.auto.tfvars
