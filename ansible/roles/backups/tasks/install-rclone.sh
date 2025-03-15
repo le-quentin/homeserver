98eee3b13de2c4ddf9c76c0b2b1bcf2cafc12977
Quentin Bonnet <quentin@bonnet.software>
2025-03-15 23:08:46 +0100

    Update rclone version when it changes

--- ansible/roles/backups/tasks/install-rclone.sh
+++ ansible/roles/backups/tasks/install-rclone.sh
@@ -6,13 +6,16 @@ version=$2
 
 cd /tmp
 
+# Cleanup any previous installation files
+rm -rf "rclone-v$version-linux-$arch.zip" "rclone-v$version-linux-$arch"
+
 # Download and unzip
 curl -O "https://downloads.rclone.org/v$version/rclone-v$version-linux-$arch.zip"
 unzip "rclone-v$version-linux-$arch.zip"
-cd rclone-*-linux-$arch
+cd "rclone-v$version-linux-$arch"
 
 # Copy binary file
-sudo cp rclone /usr/local/bin/
+sudo cp ./rclone /usr/local/bin/
 sudo chown "root:root" /usr/local/bin/rclone
 sudo chmod 755 /usr/local/bin/rclone
 
