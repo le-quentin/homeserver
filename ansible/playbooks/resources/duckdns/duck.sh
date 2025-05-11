d876de76cc015ca9c915238d9e537e4162a77bc7
Quentin Bonnet <quentin@bonnet.software>
2025-05-11 17:46:40 +0200

    Massive simplification, back to docker compose and traefik in staging

--- ansible/playbooks/resources/duckdns/duck.sh
+++
@@ -1,1 +0,0 @@
-echo url="https://www.duckdns.org/update?domains=bonnetis&token=$1&ip="| curl -v -k -K - 
---
+++ terraform/staging/apps_mainhost/main.tf
@@ -0,0 +1,41 @@
+module "k3s_node" {
+  source = "../../modules/vm"
+
+  //TODO use local datasource to communicate module outputs (https://developer.hashicorp.com/terraform/language/backend/local)
+  images = {
+    debian = "local:iso/debian-12-generic-amd64.img"
+  }
+
+  vm_id       = 2010
+  name        = "apps-host"
+  description = "The main host for homeserver apps"
+
+  environment = "staging"
+  pool        = "staging"
+
+  //TODO use local datasource to communicate module outputs (https://developer.hashicorp.com/terraform/language/backend/local)
+  bridge_lan_interface  = "vmbr1010"
+  bridge_lan_network_ip = "10.142.10.1"
+
+  # usb_mappings = [
+  #   { device_id = "toto" }
+  # ]
+
+  vm_cpu_cores        = 2
+  vm_dedicated_memory = 2048
+  vm_floating_memory  = 2048
+
+  vm_params = {
+    address   = var.vm_address
+    disk_size = 30
+    dns       = "10.142.1.2"
+    user = {
+      ssh_key  = var.vm_ssh_key
+      username = var.vm_username
+    }
+  }
+
+  custom_python = {
+    enabled = true
+  }
+}
